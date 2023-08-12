5045b6ee0e306725f8acb471495ea4dec7d719d4
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-08-12 21:01:07 +0100

    chore: task 1

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Markdown to HTML
-
-## Description
-
-This is a simple script that converts markdown files to HTML files. It uses python to convert the markdown to HTML.
+Test
---
+++ markdown2html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Markdown to HTML"""
+
+import sys
+import os.path
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
