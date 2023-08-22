40194ed2c168fe3cb111a3b2bf2fed1e320e3dca
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-08-22 07:10:59 +0100

    chore: document file

--- markdown2html.py
+++ markdown2html.py
@@ -1,39 +1,55 @@
 #!/usr/bin/python3
+"""
+This script converts a markdown file to an html file
+
+Usage: ./markdown2html.py <input_file> <output_file>
+"""
+
 import sys
 import os
-"""This script converts a markdown file to an html file"""
+
+# Function to parse the number of '#' characters at the beginning of the line
+
 
 def parse_heading(line):
-    """Returns the number of '#' characters at the beginning of the line"""
     count = 0
     while line[count] == '#':
         count += 1
     return count
 
+# Function to parse the number of '*' or '-' characters at the beginning of the line
+
+
 def parse_list(line):
-    """Returns the number of '*' or '-' characters at the beginning of the line"""
     count = 0
     while line[count] == '*' or line[count] == '-':
         count += 1
     return count
 
+# Function to parse the number of digits at the beginning of the line for ordered lists
+
+
 def parse_ordered_list(line):
-    """Returns the number of digits at the beginning of the line"""
     count = 0
     while line[count].isdigit() or line[count] == '.':
         count += 1
     return count
 
+# Function to check if a line is empty
+
+
 def is_empty(line):
-    """Returns True if the line is empty, False otherwise"""
     return not line.strip()
 
+# Function to convert a markdown file to an html file
+
+
 def convert_markdown_to_html(input_file, output_file):
-    """Converts a markdown file to an html file"""
     if not os.path.exists(input_file):
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
 
+    # Read markdown content from the input file
     with open(input_file, 'r') as md_file:
         markdown_lines = md_file.readlines()
 
@@ -43,11 +59,11 @@ def convert_markdown_to_html(input_file, output_file):
     in_paragraph = False
     previous_line_empty = False
 
-    """Iterate through each line of the markdown file"""
+    # Iterate through each line of the markdown file
     for line in markdown_lines:
         line = line.strip()
 
-        """If the line is empty, close the list or paragraph if necessary"""
+        # If the line is empty, close the list or paragraph if necessary
         if is_empty(line):
             if in_list:
                 html_lines.append(f"</{list_type}>")
@@ -111,23 +127,27 @@ def convert_markdown_to_html(input_file, output_file):
                     in_paragraph = True
                 line_with_br = line.replace('\n', '<br/>')
                 html_lines.append(line_with_br)
-    """Close the list or paragraph if necessary"""
+
+    # Close the list or paragraph if necessary
     if in_list:
         html_lines.append(f"</{list_type}>")
     elif in_paragraph:
         html_lines.append("</p>")
 
+    # Write the generated HTML content to the output file
     with open(output_file, 'w') as html_file:
         html_file.write('\n'.join(html_lines))
 
-"""Main function"""
+
+# Main function
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+        sys.stderr.write(
+            "Usage: ./markdown2html.py <input_file> <output_file>\n")
         sys.exit(1)
-    
+
     input_file = sys.argv[1]
     output_file = sys.argv[2]
-    
+
     convert_markdown_to_html(input_file, output_file)
     sys.exit(0)
